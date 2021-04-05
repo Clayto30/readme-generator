6e838bfeae42835f2bbe887de899d97fd780e1ef
Clayton Goff <cltngoff@gmail.com>
2021-04-04 21:27:35 -0700

    Readme updated files organized

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-# Professional README Generator Starter Code
+# Read Me Generator
 
-[How to create a Professional README](./readme-guide.md)
+## Description 
+
+As we all know, a great README is a key part to a great app. As such, I 
+have endeavored to demonstrate my developing skills of using Node and the 
+Inquirer Package to help developers use the Command Line Interface to make
+their own quality read me's for their own projects. 
+
+Check this out on my GitHub Repository, [Read Me Generator](https://github.com/Clayto30/readme-generator)
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+Clone the Repo onto your local device. Have you installed node.js? You will need
+that and the Inquirer Package to run this. After that, you will use the command line
+to get to crafting a wonderful README 
+
+## Usage 
+
+Once you have cloned the Repo and have the necessary things installed, type 
+"node index" into the command line when you are in the directory with the index js
+file.
+
+Follow the prompts. Take some care to input things correctly, clearly, and accurately.
+This can only help you in the long run, as it will be your README! I hope you use it
+well.
+
+```md
+![alt text](assets/images/screenshot.png)
+```
+
+
+## Credits
+
+As usual, I have to thank my instructors and TA's at the UC Berkeley Bootcamp.
+They provide the instruction for this challenge. I couldn't even begin a project 
+such as this without that vital guidance.
+
+## License
+
+[MIT License](https://choosealicense.com/licenses/mit/)
+
+
+---
+
+## Badges
+
+![badmath](https://shields.io/category/licensehttps://shields.io/category/license)
--- index.js
+++ index.js
@@ -1,96 +1,115 @@
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
+  {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter the title of your project.');
+          return false;
+        }
+      }
+    },
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-        validate: titleInput => {
-          if (titleInput) {
+      type: 'input',
+      name: 'description',
+      message: 'Please enter a description of your project.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Plese include usage information.'
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are the contribution guidelines for this project?'
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Instruct us on some test for this project, please.'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose the open source license that is right for your project',
+      choices: ['APACHE', 'MIT', 'GNU GPLv3']
+  },
+  {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub user name?',
+      validate: userNameInput => {
+          if (userNameInput) {
             return true;
           } else {
-            console.log('Please enter the title of your project.');
+            console.log('Please enter your GitHub user name.');
             return false;
           }
-        }
-      },
+      }
+  },
       {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter a description of your project.'
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions?'
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Plese include usage information.'
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'What are the contribution guidelines for this project?'
-      },
-      {
-        type: 'input',
-        name: 'test',
-        message: 'Instruct us on some test for this project, please.'
-      },
-      {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Please choose the open source license that is right for your project',
-        choices: ['APACHE', 'MIT', 'GNU GPLv3']
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub user name?',
-        validate: userNameInput => {
-            if (userNameInput) {
-              return true;
-            } else {
-              console.log('Please enter your GitHub user name.');
-              return false;
-            }
-        }
-    },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-            validate: emailInput => {
-                if (emailInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your email address.');
-                  return false;
-                }
-            }
-    },
+          type: 'input',
+          name: 'email',
+          message: 'What is your email address?',
+          validate: emailInput => {
+              if (emailInput) {
+                return true;
+              } else {
+                console.log('Please enter your email address.');
+                return false;
+              }
+          }
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-}
+function writeToFile() {
+    fs.writeFile(`check.md`, generateMarkdown(answers), err => {
+        if (err) throw err;
+      });
+    };
 
 // TODO: Create a function to initialize app
 function init() {
+  console.log(
     `
     ===============
     Write a README!
     ===============
-    `
+    `);
 
-    return inquirer
+    inquirer
         .prompt(questions)
-}
+        .then(answers => {
+          console.log(answers.title, answers.email)
+        })
+        .then(writeToFile)
+        .catch(error => {
+          if(error.isTtyError) {
+            console.log("error 1")
+          } else {
+            console.log("error 2")
+          }
+        });
+        
+        
+  };
 
 // Function call to initialize app
 init();
+
+module.exports = writeToFile;
